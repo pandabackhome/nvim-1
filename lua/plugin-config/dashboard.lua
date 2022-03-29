8a46c64c6e0467c53b171260360bc14b65eb09d3
xietianpei <xietianpei@trunk.tech>
2022-03-29 16:23:55 +0800

    adjust dashboard shortcut

--- lua/plugin-config/dashboard.lua
+++ lua/plugin-config/dashboard.lua
@@ -6,9 +6,9 @@ vim.g.dashboard_custom_section = {
   b = { description = { "  Recently files        " }, command = "Telescope oldfiles" },
   c = { description = { "  Edit Projects         " }, command = "edit ~/.local/share/nvim/project_nvim/project_history", },
   d = { description = { "  Edit init.lua         " }, command = "edit ~/.config/nvim/init.lua" },
-  e = { description = { "  Edit .bashrc          " }, command = "edit ~/.bashrc" },
-  -- f = { description = { "  Find file             " }, command = 'Telescope find_files'},
-  -- g = { description = { "  Find text             " }, command = 'Telescope live_grep'},
+  e = { description = { "  Find file             " }, command = 'Telescope find_files'},
+  f = { description = { "  Find text             " }, command = 'Telescope live_grep'},
+  -- g = { description = { "  Edit .bashrc          " }, command = "edit ~/.bashrc" },
   -- h = { description = { "  Edit keybindings      " }, command = "edit ~/.config/nvim/lua/keybindings.lua" },
   -- i = { description = { "  Change colorscheme    " }, command = "ChangeColorScheme" },
 }
